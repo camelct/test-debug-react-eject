9f59738d6d17827cdbbad2cb446a15adf42f9909
chentuo <821982911@qq.com>
2022-10-25 17:12:18 +0800

    chore: debug with react source map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(1)
+  React.useEffect(() => {
+    console.log('test-111', 111);
+    setCount(c => c + 1)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{count}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
